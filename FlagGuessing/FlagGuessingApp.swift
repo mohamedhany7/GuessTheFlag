f48149b1a025f9ad65e3c086ac34be93649e42cf
mohamedhany7 <mohameddhanyy7@gmail.com>
2022-06-09 00:34:42 +0200

    Initial Commit

---
+++ FlagGuessing/FlagGuessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuessingApp.swift
+//  FlagGuessing
+//
+//  Created by Mohamed Hany on 09/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
